9108833c6ff35fa67c44aab273577b4861647ad6
讃岐航 <sanuki@sanukikou-no-MacBook-Pro.local>
2016-04-10 23:21:13 +0900

    First Commit

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+    private const int maxTimeValue = 5999;
+
+    [SerializeField] private UIManager uiManager;
+    private float restTime;
+    private bool isRunning;
+
+    // 初期化
+    void Awake(){
+        restTime = 0;
+        UpdateTime();
+    }
+
+    // タイマーの更新
+    void Update(){
+        if(isRunning){
+            restTime -= Time.deltaTime;
+            if(restTime <= 0){
+                uiManager.FinishTimer();
+                StopTimer();
+            }
+            UpdateTime();
+        }
+    }
+
+    // フッターのボタンの挙動
+    public void FooterButton(){
+        if(isRunning){
+            StopTimer();
+        } else {
+            StartTimer();
+        }
+        UpdateTime();
+    }
+
+    // タイマーのスタート
+    public void StartTimer(){
+        isRunning = true;
+        uiManager.StartTimer();
+    }
+
+    // タイマーの一時停止
+    public void PauseTimer(){
+        isRunning = false;
+    }
+
+    // タイマーのストップ
+    public void StopTimer(){
+        PauseTimer();
+        restTime = 0;
+        uiManager.StopTimer();
+    }
+
+    // タイマーの更新
+    private void UpdateTime(){
+        uiManager.UpdateTime(restTime);
+    }
+
+    // タイマーの時間設定
+    public void AddSec(int value){
+        restTime += value;
+        restTime = Mathf.Clamp(restTime, 0, maxTimeValue);
+        UpdateTime();
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField] private Text timerText;
+    [SerializeField] private Text footerButtonText;
+    [SerializeField] private GameObject timerSettingButtons;
+    [SerializeField] private GameObject finishPanel;
+
+    public void UpdateTime(float time){
+        timerText.text = string.Format("{0:00}:{1:00}", (int)(time / 60), time % 60);
+    }
+
+    public void StartTimer(){
+        timerSettingButtons.SetActive(false);
+        footerButtonText.text = "STOP";
+    }
+
+    public void StopTimer(){
+        timerSettingButtons.SetActive(true);
+        footerButtonText.text = "START";
+    }
+
+    public void FinishTimer(){
+        finishPanel.SetActive(true);
+    }
+
+    public void CloseFinishPanel(){
+        finishPanel.SetActive(false);
+    }
+}
